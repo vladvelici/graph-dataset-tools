95934e707dd8da6514742e60b761a56f07511c18
Vlad Velici <vlad.velici@gmail.com>
2015-02-15 15:10:22 +0000

    CSV read/write code in the format that we use

---
+++ util/csvtools.go
@@ -0,0 +1,66 @@
+package util
+
+// This file provides custom CSV readers and writers, using the stdlib
+// CSV reader and writer.
+// Since most of the tools read a format (int, int[, something, ...])
+// it is useful to have the int to string conversons in one place so
+// I don't repeat myself too much.
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+// Write is a CSV custom writer type.
+type Writer struct {
+	w *csv.Writer
+}
+
+// NewWriter creates a new writer.
+func NewWriter(f io.Writer) *Writer {
+	return &Writer{csv.NewWriter(f)}
+}
+
+// Write writes a CSV record a,b,rubbish...
+func (w *Writer) Write(a, b int, rubbish []string) error {
+	line := make([]string, len(rubbish)+2)
+	line[0] = strconv.Itoa(a)
+	line[1] = strconv.Itoa(b)
+	copy(line[2:], rubbish)
+	return w.w.Write(line)
+}
+
+// Reader is a custom CSV Reader.
+type Reader struct {
+	r *csv.Reader
+}
+
+// NewReader creates new CSV reader.
+func NewReader(r io.Reader) *Reader {
+	return &Reader{csv.NewReader(r)}
+}
+
+// Read reads an element.
+func (r *Reader) Read() (int, int, []string, error) {
+	record, err := r.r.Read()
+	if err != nil {
+		return 0, 0, nil, err
+	}
+	if len(record) < 2 {
+		return 0, 0, record, fmt.Errorf("Not enough data in the record.")
+	}
+	a, err := strconv.Atoi(record[0])
+	if err != nil {
+		return 0, 0, record, err
+	}
+	b, err := strconv.Atoi(record[1])
+	if err != nil {
+		return 0, 0, record, err
+	}
+	if len(record) == 2 {
+		return a, b, nil, nil
+	}
+	return a, b, record[2:], nil
+}
